dd5991cfdaa19cf7c4202d28cda1e787e8acfa06
Mazen Essam <mazenessam2011@outlook.com>
2026-08-22 16:19:22 +0300

    remove api call

--- app/(auth)/create-user/page.tsx
+++ app/(auth)/create-user/page.tsx
@@ -1,25 +1,23 @@
-// "use client";
+"use client";
 
-// import { authClient } from "@/lib/auth/auth-client";
+import { authClient } from "@/lib/auth/auth-client";
 
-// export default function CreateUserPage() {
-//   async function createUser() {
-//     const { data, error } = await authClient.signUp.email({
-//       name: "Tire Depot",
-//       email: "test@tiredepot.com",
-//       password: "Depot@123",
-//       company: "E02",
-//     });
+export default function CreateUserPage() {
+  async function createUser() {
+    // const { data, error } = await authClient.signUp.email({
+    //   name: "Tire Depot",
+    //   email: "test@tiredepot.com",
+    //   password: "Depot@123",
+    //   company: "E02",
+    // });
+    // if (error) {
+    //   console.error("Error:", error);
+    //   alert(error.message);
+    //   return;
+    // }
+    // console.log("Created user:", data);
+    // alert("User created!");
+  }
 
-//     if (error) {
-//       console.error("Error:", error);
-//       alert(error.message);
-//       return;
-//     }
-
-//     console.log("Created user:", data);
-//     alert("User created!");
-//   }
-
-//   return <button onClick={createUser}>Create User</button>;
-// }
+  return <button onClick={createUser}>Create User</button>;
+}
